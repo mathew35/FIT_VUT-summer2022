2af9469672ab6096e61e1b037db17dcf56a4c433
mathew35 <matus549@gmail.com>
2022-04-12 21:58:02 +0200

    IZU correction

--- IZU/Project_2/xvrabl05.pl
+++ IZU/Project_2/xvrabl05.pl
@@ -9,6 +9,7 @@
 u39_1:- u39([4,-3,1,8,3,8,21],12,2,VOUT),write(VOUT).	% 8
 u39_2:- u39([4.56],12,2,VOUT),write(VOUT).		% 4.56
 u39_3:- u39([],12,2,VOUT),write(VOUT).			% 0
+u39_4:- u39([2,-5,0,26,14,3,-1],12,1,VOUT),write(VOUT).
 u39_r:- write('Zadej LIN: '),read(LIN),
 	write('Zadej VIN1: '),read(VIN1),
 	write('Zadej VIN2: '),read(VIN2),
@@ -18,4 +19,4 @@ maxlist([X,Y|Z],M,MAX,MIN):- maxlist([Y|Z],Mrest,MAX,MIN),max(X,Mrest,M,MAX,MIN)
 maxlist([X],Z,MAX,MIN):- MAX>X,MIN<X,Z is X;Z is 0.
 maxlist([],0,MAX,MIN). 
 max(X,Y,X,MAX,MIN):- X>=Y,MAX>X,MIN<X.
-max(X,Y,Y,MAX,MIN):- X<Y.
+max(X,Y,Y,MAX,MIN).
